0668a0167f587d57a48a32f7235ec39c60fe5715
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-06-16 20:26:48 -0300

    game completed

--- solver.cpp
+++ solver.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-
+#include <vector>
+#include <functional>
 /*
 g++ -Wall -Wextra -Werror solver.cpp -o app.exe -lsfml-graphics -lsfml-window -lsfml-system
 .\app.exe
@@ -60,10 +61,99 @@ struct Bubble {
     }
 };
 
+struct Board {
+    sf::RenderWindow& window;
+    std::vector<Bubble> bubbles;
+    Pincel pencil;
+    int hits { 0 };
+    int misses { 0 };
+
+    Board(sf::RenderWindow& window) : window(window), pencil(window) {
+        bubbles.push_back(Bubble(100, 100, 'A', 1));
+        bubbles.push_back(Bubble(200, 100, 'B', 2));
+        bubbles.push_back(Bubble(300, 100, 'C', 3));
+    }
+
+    void update() {
+        if (this->check_new_bubble()) {
+            this->add_new_bubble();
+        }
+
+        for (Bubble& bubble : bubbles) {
+            bubble.update();
+        }
+
+        this->mark_outside_bubbles();
+        this->remove_dead_bubbles();
+    }
+
+    void remove_dead_bubbles() {
+        std::vector<Bubble> vivas;
+        for (Bubble& bubble : bubbles) {
+            if (bubble.alive) {
+                vivas.push_back(bubble);
+            }
+        }
+        this->bubbles = vivas;
+    }
+
+    void mark_outside_bubbles() {
+        for (Bubble& bubble : bubbles) {
+            if (bubble.y + 2 * Bubble::radius > (int) this->window.getSize().y && bubble.alive) {
+                bubble.alive = false;
+                this->misses++;
+            }
+        }
+    }
+
+    void mark_by_hit(char letter) {
+        for (Bubble& bubble : bubbles) {
+            if (bubble.letter == letter) {
+                bubble.alive = false;
+                this->hits++;
+                break;
+            }
+        }
+    }
+
+    bool check_new_bubble() {
+        static const int new_bubble_timeout { 30 };
+        static int new_bubble_timer { 0 };
+
+        new_bubble_timer--;
+        if (new_bubble_timer <= 0) {
+            new_bubble_timer = new_bubble_timeout;
+            return true;
+        }
+        return false;
+    }
+
+    void add_new_bubble() {
+        int x = rand() % ((int) this->window.getSize().x - 2 * Bubble::radius);
+        int y = - 2 * Bubble::radius;
+        int speed = rand() % 5 + 1;
+        char letter = rand() % 26 + 'A';
+        bubbles.push_back(Bubble(x, y, letter, speed));
+    }
+
+    void draw() {
+        pencil.write("Hits: " + std::to_string(this->hits) + " Misses: " + std::to_string(this->misses), 10, 10, 20, sf::Color::White);
+        pencil.write("Size: " + std::to_string(this->bubbles.size()), 10, 30, 20, sf::Color::White);
+        for (Bubble& bubble : bubbles) {
+            bubble.draw(window);
+        }
+    }
+};
+
 struct Game {
     sf::RenderWindow window;
+    Board board;
+    std::function<void()> on_update;
 
-    Game(): window(sf::VideoMode(800, 600), "Bubbles") {
+    Game(): window(sf::VideoMode(800, 600), "Bubbles"), board(window) {
+        this->on_update = [&]() {
+            this->gameplay();
+        };
         window.setFramerateLimit(60);
     }
 
@@ -73,23 +163,39 @@ struct Game {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+            else if (event.type == sf::Event::TextEntered) {
+                char code = static_cast<char>(event.text.unicode);
+                code = toupper(code);
+                board.mark_by_hit(code);
+            }
         }
     }
 
-    void draw() {
-        window.clear();
+    void gameplay() {
+        board.update();
 
-        static Bubble bubble(200, 100, 'B', 2);
-        bubble.update();
-        bubble.draw(window);
+        window.clear(sf::Color::Black);
+        board.draw();
+        window.display();
 
+        if (board.misses > 10) {
+            this->on_update = [&]() {
+                this->gameover();
+            };
+        }
+    }
+
+    void gameover() {
+        static Pincel pincel(window);
+        window.clear(sf::Color::Red);
+        pincel.write("Game Over", 400, 300, 50, sf::Color::White);
         window.display();
     }
 
     void main_loop() {
         while (window.isOpen()) {
             process_events();
-            draw();
+            on_update();
         }
     }
 };
